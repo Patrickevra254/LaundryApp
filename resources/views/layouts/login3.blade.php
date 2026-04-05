506ebd8c867245ece78ea9da7fb5e29daa7f4acb
Patrickevra254 <patrickevra254@gmail.com>
2026-04-05 20:56:03 +0100

    favicon title update

--- resources/views/layouts/login3.blade.php
+++ resources/views/layouts/login3.blade.php
@@ -864,7 +864,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LaundryPro – Sign In</title>
+    <title>Tolbeel Sign In | Sign Up</title>
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
